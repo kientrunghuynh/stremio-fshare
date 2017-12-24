2e35c31399e722f3dbb2d4a630efda72847caf32
Trung.Huynh <trung.huynh@adurolife.com>
2017-12-25 00:27:35 +0700

    test

--- index.js
+++ index.js
@@ -1,62 +1,102 @@
 var Stremio = require("stremio-addons");
+var magnet = require("magnet-uri");
 
-// Enable server logging for development purposes
-process.env.STREMIO_LOGGING = true; 
+process.env.STREMIO_LOGGING = true; // enable server logging for development purposes
 
-// Define manifest object
 var manifest = { 
-    // See https://github.com/Stremio/stremio-addons/blob/master/docs/api/manifest.md for full explanation
-    id: "org.stremio.sf",
-    version: "1.0.0",
+    "id": "org.stremio.fshare",
+    "version": "1.0.0",
 
-    name: "SFshare",
-    description: "fshare",
-    icon: "https://www.iconfinder.com/icons/2799202/download/png/256", 
-    background: "http://dark.pozadia.org/images/wallpapers/18807153/Dark/Gothic%20Architecture%20Building.jpg",
+    "name": "Fshare Addon",
+    "description": "Sample addon providing a few public domain movies",
+    "icon": "URL to 256x256 monochrome png icon", 
+    "background": "URL to 1366x756 png background",
 
     // Properties that determine when Stremio picks this add-on
-    types: ["movie","series","tv","channel"], // your add-on will be preferred for those content types
-    idProperty: ['imdb_id'], // the property to use as an ID for your add-on; your add-on will be preferred for items with that property; can be an array
+    "types": ["movie", "series"], // your add-on will be preferred for those content types
+    "idProperty": "imdb_id", // the property to use as an ID for your add-on; your add-on will be preferred for items with that property; can be an array
     // We need this for pre-4.0 Stremio, it's the obsolete equivalent of types/idProperty
-    filter: { "query.imdb_id": { "$exists": true }, "query.type": { "$in": ["movie","series","tv","channel"] } },
-
-    // Adding a sort would add a tab in Discover and a lane in the Board for this add-on
-    sorts: [ {prop: "popularities.sfshare", name: "SFshare", types: ["movie","series","tv","channel"]}],
+    "filter": { "query.imdb_id": { "$exists": true }, "query.type": { "$in":["series","movie"] } }
 };
 
-var dataset = {};
+var dataset = {
+    // For p2p streams, you can provide availability property, from 0 to 3, to indicate stability of the stream; if not passed, 1 will be assumed
+    // mapIdx is the index of the file within the torrent ; if not passed, the largest file will be selected
+    "tt0032138": { infoHash: "24c8802e2624e17d46cd555f364debd949f2c81e", mapIdx: 0, availability: 2 }, // the wizard of oz 1939
+    "tt0017136": { infoHash: "dca926c0328bb54d209d82dc8a2f391617b47d7a", mapIdx: 1, availability: 2 }, // metropolis, 1927
+    //"tt0017136": { url: "ipfs://QmYbmoDHDNgqoDbLC4vMyoMH5dYQdrxL1wh63x9rhSC6Zf/avchd-metropolis.1927.1080p.mkv" }, // IPFS example - metropolis, 1927, restored 2010 
+    
+    // night of the living dead, example from magnet
+    "tt0063350": fromMagnet("magnet:?xt=urn:btih:A7CFBB7840A8B67FD735AC73A373302D14A7CDC9&dn=night+of+the+living+dead+1968+remastered+bdrip+1080p+ita+eng+x265+nahom&tr=udp%3A%2F%2Ftracker.publicbt.com%2Fannounce&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce"), // night of the living dead, 1968
+    "tt0051744": { infoHash: "9f86563ce2ed86bbfedd5d3e9f4e55aedd660960" }, // house on haunted hill 1959
+
+    "tt1254207": { url: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4", availability: 1 }, // big buck bunny, HTTP stream
+    "tt0031051": { yt_id: "m3BKVSpP80s", availability: 2 }, // The Arizona Kid, 1939; YouTube stream
+
+    "tt0137523": { externalUrl: "https://www.netflix.com/watch/26004747" }, // Fight Club, 1999; redirect to Netflix
+
+    "tt1748166 1 1": { infoHash: "07a9de9750158471c3302e4e95edb1107f980fa6" }, // Pioneer One
+};
+
+// utility function to add from magnet
+function fromMagnet(uri) {
+    var parsed = magnet.decode(uri);
+    var infoHash = parsed.infoHash.toLowerCase();
+    var tags = [];
+    if (uri.match(/720p/i)) tags.push("720p");
+    if (uri.match(/1080p/i)) tags.push("1080p");
+    return {
+        infoHash: infoHash,
+        sources: (parsed.announce || []).map(function(x) { return "tracker:"+x }).concat(["dht:"+infoHash]),
+        tag: tags,
+        title: tags[0], // show quality in the UI
+        availability: 2, 
+    }
+}
 
 var methods = { };
+var addon = new Stremio.Server(methods, manifest);
 
-var addon = new Stremio.Server({
-    "stream.find": function(args, callback) {
-        console.log("received request from stream.find", args)
-        // callback expects array of stream objects
-    },
-    "meta.find": function(args, callback) {
-        console.log("received request from meta.find", args)
-        // callback expects array of meta object (primary meta feed)
-        // it passes "limit" and "skip" for pagination
-    },
-    "meta.get": function(args, callback) {
-        console.log("received request from meta.get", args)
-        // callback expects one meta element
-    },
-    "meta.search": function(args, callback) {
-        console.log("received request from meta.search", args)
-        // callback expects array of search results with meta objects
-        // does not support pagination
-    },
-}, manifest);
+if (module.parent) {
+    module.exports = addon
+} else {
+    var server = require("http").createServer(function (req, res) {
+        addon.middleware(req, res, function() { res.end() }); // wire the middleware - also compatible with connect / express
+    }).on("listening", function()
+    {
+        console.log("Sample Stremio Addon listening on "+server.address().port);
+    }).listen(process.env.PORT || 7000);
+}
 
-if (require.main===module) var server = require("http").createServer(function (req, res) {
-    addon.middleware(req, res, function() { res.end() }); // wire the middleware - also compatible with connect / express
-}).on("listening", function()
-{
-    var port = server.address().port;
-    console.log("Sample Stremio Addon listening on "+port);
-    console.log("You can test this add-on via the web app at: http://app.strem.io/#/discover/movie?addon="+encodeURIComponent('http://localhost:'+port))
-}).listen(process.env.PORT || 7000);
 
-// Export for local usage
-module.exports = addon;
+// Streaming
+methods["stream.find"] = function(args, callback) {
+    if (! args.query) return callback();
+
+    //callback(null, [dataset[args.query.imdb_id]]); // Works only for movies
+    
+    var key = [args.query.imdb_id, args.query.season, args.query.episode].filter(function(x) { return x }).join(" ");
+    callback(null, [dataset[key]]);
+};
+
+// Add sorts to manifest, which will add our own tab in sorts
+manifest.sorts = [{prop: "popularities.helloWorld", name: "Hello World",types:["movie","series"]}];
+
+// To provide meta for our movies, we'll just proxy the official cinemeta add-on
+var client = new Stremio.Client();
+client.add("http://cinemeta.strem.io/stremioget/stremio/v1");
+
+// Proxy Cinemeta, but get only our movies
+// That way we get a tab "Hello World" with the movies we provide :) 
+methods["meta.find"] = function(args, callback) {
+    var ourImdbIds = Object.keys(dataset).map(function(x) { return x.split(" ")[0] });
+    args.query = args.query || { };
+    args.query.imdb_id = args.query.imdb_id || { $in: ourImdbIds };
+    client.meta.find(args, function(err, res) {
+	if (err) console.error(err);
+        callback(err, res ? res.map(function(r) { 
+            r.popularities = { helloWorld: 10000 }; // we sort by popularities.helloWorld, so we should have a value
+            return r;
+        }) : null);
+    });
+}
